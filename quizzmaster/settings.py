2c130af9dbecce8f5ac78195730e0b51fb169fff
AtufaShireen <atufashireen@gmail.com>
2021-04-16 20:20:21 +0530

    addded whitenoise4

--- quizzmaster/settings.py
+++ quizzmaster/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -115,4 +116,4 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'quiz:quizzes'
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
